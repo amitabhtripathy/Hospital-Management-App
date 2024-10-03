78a8bb88b71e64d9eb31c11c1f0774bac7707d14
amitanshu <144086900+amitanshu3@users.noreply.github.com>
2024-10-03 23:56:03 +0530

    added update

--- backend/app.js
+++ backend/app.js
@@ -4,37 +4,57 @@ import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import fileUpload from "express-fileupload";
+import cloudinary from "cloudinary";
 import { errorMiddleware } from "./middlewares/error.js";
 import messageRouter from "./router/messageRouter.js";
 import userRouter from "./router/userRouter.js";
 import appointmentRouter from "./router/appointmentRouter.js";
 
-const app = express();
+// Load environment variables from .env file
 config({ path: "./config.env" });
 
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
-    method: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
-  })
-);
+// Initialize the Express application
+const app = express();
 
+// Configure CORS
+const corsOptions = {
+  origin: [process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+// Middleware
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(
   fileUpload({
     useTempFiles: true,
     tempFileDir: "/tmp/",
   })
 );
+
+// Cloudinary Configuration
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Database connection
+dbConnection();
+
+// Route handlers
 app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/appointment", appointmentRouter);
 
-dbConnection();
-
+// Error handling middleware
 app.use(errorMiddleware);
-export default app;
+
+// Start the server
+const PORT = process.env.PORT || 5000; // Default to 5000 if PORT is not defined
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}`);
+});
